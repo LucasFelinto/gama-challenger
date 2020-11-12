68a89221a04b22ec2ffad83922f96fdf2d7ee182
Souza <tacio.antonio.souza@accenture.com>
2020-11-12 16:16:51 -0300

    Corrections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { TicketComponent } from './pages/ticket/ticket.component';
 import { UserComponent } from './pages/user/user.component';
 
@@ -9,11 +8,7 @@ const routes: Routes = [
   {
     path: '',
     component: HomepageComponent
-  }, 
-  {
-    path: 'cadastro',
-    component: SignUpComponent
-  },  
+  },
   {
     path: 'perfil',
     component: UserComponent
