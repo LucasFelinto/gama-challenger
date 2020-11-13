c34260f4347d8ff1c3c4c2b78d2800c85497987c
LucasFelinto <elucasfelinto@gmail.com>
2020-11-13 17:08:28 -0300

    Corrigindo chamada AJAX

--- src/app/pages/ticket/ticket.component.ts
+++ src/app/pages/ticket/ticket.component.ts
@@ -1,35 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { TicketService } from './ticket.service';
 
+interface Ticket {
+  id: number;
+  flightGet: string;
+}
+
 @Component({
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
-  styleUrls: ['./ticket.component.scss']
+  styleUrls: ['./ticket.component.scss'],
 })
-
-export interface Ticket {
-
-}
-
-
 export class TicketComponent implements OnInit {
+  constructor(private ticketService: TicketService) {}
 
-  constructor(private ticketService: TicketService) { }
+  tickets;
 
   ngOnInit(): void {
     this.getTicket();
   }
 
   getTicket() {
-    this.ticketService.getTicket().subscribe(result => {
-      result.map(ticket => {
-        const newObject = {...ticket}
-        newObject.flightGet = JSON.parse(ticket.flightGet);
-        return newObject; 
-      });
-    },
-     erro => console.log(erro),
+    this.ticketService.getTicket().subscribe(
+      (result: Array<Ticket>) => {
+        this.tickets = result.map((ticket) => {
+          const newObject = { ...ticket };
+          newObject.flightGet = JSON.parse(ticket.flightGet);
+          return newObject;
+        });
+        console.log(this.tickets);
+      },
+      (erro) => console.log(erro)
     );
   }
-
 }
--- src/app/pages/ticket/ticket.service.ts
+++ src/app/pages/ticket/ticket.service.ts
@@ -2,25 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TicketService {
-
-  constructor(private httpClient: HttpClient) { 
-  }
+  constructor(private httpClient: HttpClient) {}
   url = 'https://javatravelers-backend.azurewebsites.net/';
 
-
-  public getTicket(){
-      return this.httpClient.get(`${this.url}tickets/list`, 
-      {
-        headers: new HttpHeaders({
-          'Content-Type':  'application/json',
-          'Authorization' : 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYXJjb3Ntb250ZWlyb0BnbWFpbC5jb20iLCJzY29wZXMiOiJST0xFX1VTRVIiLCJpYXQiOjE2MDUyOTA4MTksImV4cCI6MTYwNTI5NDQxOX0.iToxEpJjiZwGcz0GlDpe-cDTnInkbEMSkF3yeRKvHbg'
-        })
-      }
-    );
+  public getTicket() {
+    return this.httpClient.get(`${this.url}tickets/list`, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization:
+          'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYXJjb3Ntb250ZWlyb0BnbWFpbC5jb20iLCJzY29wZXMiOiJST0xFX1VTRVIiLCJpYXQiOjE2MDUyOTYzNjAsImV4cCI6MTYwNTI5OTk2MH0.STigeIbuYd5pAmUK4eWC-99bnsMzfWPOzEjcqJy_HOU',
+      }),
+    });
   }
-
-  
 }
