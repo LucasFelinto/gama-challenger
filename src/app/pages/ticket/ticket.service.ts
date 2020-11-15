3c72b4105678a920659ed7e2ba09cdb21e785314
João <johngabrielbarreto1337@gmail.com>
2020-11-15 16:58:34 -0300

    adicionado ticket com api de forma manual

--- src/app/pages/ticket/ticket.service.ts
+++ src/app/pages/ticket/ticket.service.ts
@@ -13,7 +13,7 @@ export class TicketService {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
         Authorization:
-          'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYXJjb3Ntb250ZWlyb0BnbWFpbC5jb20iLCJzY29wZXMiOiJST0xFX1VTRVIiLCJpYXQiOjE2MDUzMDEwNzcsImV4cCI6MTYwNTMwNDY3N30.GRGgyArSWCDmAjYKl569xXRjHzXzv468NNdGZ8WOq78',
+        "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYXJjb3Ntb250ZWlyb0BnbWFpbC5jb20iLCJzY29wZXMiOiJST0xFX1VTRVIiLCJpYXQiOjE2MDU0NjY2MjgsImV4cCI6MTYwNTQ3MDIyOH0.H9qIE7kuYqn_DhdMBO3KxwGCqOsEOujsuFPdTEjB-us",
       }),
     });
   }
