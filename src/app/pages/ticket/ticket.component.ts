dff6b81d97abf9e5ac3c56b21f29b7c3f7978ca8
João <johngabrielbarreto1337@gmail.com>
2020-11-16 13:45:27 -0300

    ticket api com frontend

--- src/app/pages/ticket/ticket.component.ts
+++ src/app/pages/ticket/ticket.component.ts
@@ -15,22 +15,35 @@ export class TicketComponent implements OnInit {
   constructor(private ticketService: TicketService) {}
 
   tickets;
-  
+
+  flightInfo;
+
+  segment;
+
+  itinerarie;
+
   ngOnInit(): void {
     this.getTicket();
   }
 
   getTicket() {
-    this.ticketService.getTicket().subscribe(
-      (result: Array<Ticket>) => {
-        this.tickets = result.map((ticket) => {
-          const newObject = { ...ticket };
-          newObject.flightGet = JSON.parse(ticket.flightGet);
-          return newObject;
-        });
-        console.log(this.tickets);
-      },
-      (erro) => console.log(erro)
-    );
+    this.ticketService.getTicket().subscribe((result) => {
+      this.tickets = result;
+      this.flightInfo = result.flightGet.data.flightOffers[0];
+      this.itinerarie = this.flightInfo.itineraries[0];
+      this.segment = this.itinerarie.segments[0];
+      console.log(this.tickets);
+    });
+    // this.ticketService.getTicket().subscribe(
+    //   (result: Array<Ticket>) => {
+    //     this.tickets = result.map((ticket) => {
+    //       const newObject = { ...ticket };
+    //       newObject.flightGet = JSON.parse(ticket.flightGet);
+    //       return newObject;
+    //     });
+    //     console.log(this.tickets);
+    //   },
+    //   (erro) => console.log(erro)
+    // );
   }
 }
--- src/app/pages/ticket/ticket.service.ts
+++ src/app/pages/ticket/ticket.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { LoginComponent } from '../login/login.component';
+import { map, take } from 'rxjs/operators';
 
 interface User {
   login: string;
   token: string;
   userId: string;
 }
-
 
 @Injectable({
   providedIn: 'root',
@@ -18,11 +17,24 @@ export class TicketService {
   url = 'https://javatravelers-backend.azurewebsites.net/';
 
   public getTicket() {
-    return this.http.get(`${this.url}tickets/list`, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: localStorage.getItem('myTokenUser'),
-      }),
-    });
+    return this.http
+      .get(`${this.url}tickets/list`, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem('myTokenUser'),
+        }),
+      })
+      .pipe(
+        map((response) => {
+          let obj;
+          console.log(response);
+          // obj = response.map((ticket) => {
+          //   const newObject = { ...ticket };
+          //   newObject.flightGet = JSON.parse(ticket.flightGet);
+          //   return newObject;
+          // });
+          return obj;
+        })
+      );
   }
 }
